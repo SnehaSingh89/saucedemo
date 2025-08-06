9ab34697e370d88b35ed797995c63ecb36b418b8
SnehaSingh89 <sneha@primeqasolutions.com>
2025-08-06 15:47:22 +0530

    initial commit

---
+++ features/steps/test_login.py
@@ -0,0 +1,41 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from pytest_bdd import scenarios, given, when, then
+from pages.login_page import LoginPage
+import os
+
+
+# Link this file to the feature file
+scenarios('login.feature')
+
+
+@pytest.fixture
+def driver():
+    options = Options()
+
+    # Default is headed, unless HEADLESS=true is set
+    if os.getenv('HEADLESS') == 'true':
+        options.add_argument('--headless')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+@given('the user is on the login page')
+def open_login_page(driver):
+    login_page = LoginPage(driver)
+    login_page.load()
+
+@when('the user logs in with username "standard_user" and password "secret_sauce"')
+def perform_login(driver):
+    login_page = LoginPage(driver)
+    login_page.login("standard_user", "secret_sauce")
+
+@then('the user should be redirected to the products page')
+def verify_login_success(driver):
+    assert "inventory" in driver.current_url
---
+++ pages/login_page.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_input = (By.ID, "user-name")
+        self.password_input = (By.ID, "password")
+        self.login_button = (By.ID, "login-button")
+
+    def load(self):
+        """Open the login page"""
+        self.driver.get("https://www.saucedemo.com/")
+
+    def enter_username(self, username):
+        self.driver.find_element(*self.username_input).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(*self.password_input).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(*self.login_button).click()
+
+    def login(self, username, password):
+        """Combined method to perform full login"""
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login()
